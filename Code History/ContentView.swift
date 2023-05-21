11291c93163ed86c8e03d9c5a7d88fe6f5c43856
Christelle Quilang <christellequilang@protonmail.com>
2023-05-21 18:36:58 +1200

    Create basic question screen

--- Code History/ContentView.swift
+++ Code History/ContentView.swift
@@ -7,15 +7,68 @@
 
 import SwiftUI
 
+let mainColor = Color(red: 20/255, green: 28/255, blue: 58/255)
+let accentColor = Color(red: 48/255, green: 105/255, blue: 240/255)
+
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        ZStack {
+            mainColor.ignoresSafeArea()
+            VStack {
+                Text("1/10")
+                    .font(.callout)
+                    .multilineTextAlignment(.leading)
+                    .padding()
+                Text("What was the first computer bug?")
+                    .font(.largeTitle)
+                    .bold()
+                    .multilineTextAlignment(.center)
+                Spacer()
+                HStack {
+                    Button(action: {
+                        print("Tapped on choice 1")
+                    }, label:{
+                        Text("Ant")
+                            .font(.body)
+                            .bold()
+                            .multilineTextAlignment(.center)
+                            .padding()
+                            .border(Color.accentColor, width: 4)
+                    })
+                    Button(action: {
+                        print("Tapped on choice 2")
+                    }, label:{
+                        Text("Beetle")
+                            .font(.body)
+                            .bold()
+                            .multilineTextAlignment(.center)
+                            .padding()
+                            .border(Color.accentColor, width: 4)
+                    })
+                    Button(action: {
+                        print("Tapped on choice 3")
+                    }, label:{
+                        Text("Moth")
+                            .font(.body)
+                            .bold()
+                            .multilineTextAlignment(.center)
+                            .padding()
+                            .border(Color.accentColor, width: 4)
+                    })
+                    Button(action: {
+                        print("Tapped on choice 4")
+                    }, label:{
+                        Text("Fly")
+                            .font(.body)
+                            .bold()
+                            .multilineTextAlignment(.center)
+                            .padding()
+                            .border(Color.accentColor, width: 4)
+                    })
+                }
+            }
         }
-        .padding()
+        .foregroundColor(.white)
     }
 }
 
