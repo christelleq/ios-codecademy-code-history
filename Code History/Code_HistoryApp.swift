0093a4ec5329ee731014e5b45ef19e1f640c71ee
Christelle Quilang <christellequilang@protonmail.com>
2023-05-21 17:57:05 +1200

    Initial Commit

---
+++ Code History/Code_HistoryApp.swift
@@ -0,0 +1,17 @@
+//
+//  Code_HistoryApp.swift
+//  Code History
+//
+//  Created by Christelle Quilang on 21/05/23.
+//
+
+import SwiftUI
+
+@main
+struct Code_HistoryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
